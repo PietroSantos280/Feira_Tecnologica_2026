bcacf31d30612f32d60e1fd60d88687686957c81
PietroSantos280 <santospietro0910@gmail.com>
2026-09-10 18:28:15 -0300

    4°Prototipo antes da apresentacao teste

--- inicio.js
+++ inicio.js
@@ -1,10 +1,11 @@
-        function filtrar() {
-            const bioma = document.getElementById('habitat-filter').value;
-            const fauna = document.getElementById('fauna-filter').value;
-            
-            if (!bioma && !fauna) {
-                alert('Por favor, selecione pelo menos uma opção para explorar!');
-            } else {
-                alert(`Buscando dados para: Bioma [${bioma || 'Todos'}] | Fauna [${fauna || 'Todas'}]`);
+        document.addEventListener('DOMContentLoaded', function () {
+            const formulario = document.getElementById('formularioContato');
+
+            if (formulario) {
+                formulario.addEventListener('submit', function (event) {
+                    event.preventDefault();
+                    alert('Obrigado pelo seu contato! Sua mensagem foi enviada com sucesso.');
+                    window.location.href = 'index.html';
+                });
             }
-        }
+        });
